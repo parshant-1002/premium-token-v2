eb854380c3fd4e93e8d6403331ff0c01e11b3740
Davinder Singh <davinder.singh@chicmic.co.in>
2024-03-22 14:45:22 +0530

    fix: styling import

--- src/shared/components/SanitizeHtml/index.jsx
+++ src/shared/components/SanitizeHtml/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import createDOMPurify from 'dompurify';
+import "./style.scss"
 
 const SafeHTML = ({ html }) => (
     <span className='render_html_safely' dangerouslySetInnerHTML={{ __html: createDOMPurify.sanitize(html) }} />
