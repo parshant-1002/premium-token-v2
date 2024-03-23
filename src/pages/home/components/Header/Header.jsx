71bedd4bf32deba01dd78fd85f0e6545f6b5c39c
Yogesh-chicmic19 <yogesh.kumar@chicmic.co.in>
2024-03-23 19:34:53 +0530

    style: header responsiveness

--- src/pages/home/components/Header/Header.jsx
+++ src/pages/home/components/Header/Header.jsx
@@ -1,11 +1,10 @@
 import { Link } from "react-router-dom";
-import "./Header.scss";
 import { addBaseUrlToUrls } from "../../../../shared/utilities";
 import { RenderIconWithHover } from "../../../../shared/components/RenderIconWithHover";
 import { ICONS } from "../../../../assets";
 import SafeHTML from "../../../../shared/components/SanitizeHtml";
 import { useEffect, useState } from "react";
-import { Icons } from "react-toastify";
+import "./Header.scss";
 
 const Header = ({ content = {} }) => {
 	const { twitter, discord, telegram, viewChartButton, airdropButton, buyTokenButton, premiumTokenTitle } = content
@@ -25,60 +24,100 @@ const Header = ({ content = {} }) => {
 		};
 	}, []);
 
+	// Hamburger Menu
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setIsOpen(!isOpen);
+	};
+
+
 	return (
 
 		<header className={isSticky ? 'header sticky-header' : 'header'}>
-			<nav className="navbar navbar-expand-xl bg-transparent py-0">
+
+			<nav className={`navbar navbar-expand-xl bg-transparent py-0 ${isOpen ? 'menu-open' : ''}`}>
 				<div className="container">
 					<Link className="navbar-brand" to={"/"}>
 						<RenderIconWithHover iconUrl={addBaseUrlToUrls(premiumTokenTitle)} hoverIconUrl={"./img-1.svg"} defaultIcon={ICONS.Logo} />
 					</Link>
-					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+					<button className="navbar-toggler" onClick={toggleMenu}>
 						<span className="navbar-toggler-icons">
 							<span></span>
 							<span></span>
 							<span></span>
 						</span>
 					</button>
-					<div className="collapse  navbar-collapse mobileNavbar" id="navbarSupportedContent">
-						<div className="main-navigation d-flex justify-content-center justify-content-between flex-column  align-items-center gap-12 ms-auto">
-							<div className="w-100 d-flex justify-content-center align-items-center position-relative">
-								<Link className="navbar-brand" to={"/"}>
-									<RenderIconWithHover iconUrl={addBaseUrlToUrls(premiumTokenTitle)} hoverIconUrl={"./img-1.svg"} defaultIcon={ICONS.Logo} />
+
+					<div className="collapse navbar-collapse">
+
+						<div className="main-navigation d-flex justify-content-center justify-content-md-between flex-column flex-md-row align-items-center gap-12 ms-auto">
+							<div className="social-icon d-flex justify-content-between align-items-center gap-12 ms-md-auto">
+								<Link className="btn_icon btn44 dark-green-gradient" rel="noreferrer">
+									<span><RenderIconWithHover iconUrl={addBaseUrlToUrls(twitter?.image)} hoverIconUrl={"./img-1.svg"} defaultIcon={ICONS.Twitter} /></span>
 								</Link>
-								<span className="CloseMenu">
-									<img width={40} height="40" className="img-fluid" src={ICONS.CloseMenu} alt="CloseIcon" />
-								</span>
+								<Link className="btn_icon btn44 dark-green-gradient" rel="noreferrer">
+									<span><RenderIconWithHover iconUrl={addBaseUrlToUrls(discord?.image)} hoverIconUrl={"./img-1.svg"} defaultIcon={ICONS.Discord} /></span>
+								</Link>
+								<Link className="btn_icon btn44 dark-green-gradient" rel="noreferrer">
+									<span>
+										<RenderIconWithHover iconUrl={addBaseUrlToUrls(discord?.image)} hoverIconUrl={"./img-1.svg"} defaultIcon={ICONS.Discord} />
+									</span>
+								</Link>
 							</div>
-							<div className="toogleBg d-block d-lg-none">
-								<img width={327} height="414" className="img-fluid" src={ICONS.ToogleMenuBg} alt="" />
-							</div>
-							<div className="mobileMenuList">
-							<div className="w-100 header-button d-flex justify-content-between flex-column-reverse align-items-center gap-16">
-									<button type="button" className="btn btn-md btn-primary w-100">
-										<span className="transform-none"><RenderIconWithHover iconUrl={addBaseUrlToUrls(telegram?.image)} hoverIconUrl={"./img-1.svg"} defaultIcon={ICONS.Airdrop} /><SafeHTML html={airdropButton?.text} /></span>
-									</button>
-									<button type="button" className="btn btn-md btn-primary w-100"><span className="transform-none"><SafeHTML html={viewChartButton?.text} /></span></button>
-									<button type="button" className="btn btn-md btn-secondary w-100"><span className="text-white transform-none"><SafeHTML html={buyTokenButton?.text} /></span></button>
-								</div>
-								<div className="brand_banner">
-									<img width={378} height="316" className="img-fluid" src={ICONS.BrandFrame} alt="Brand" />
-								</div>
-								<div className="social-icon d-flex justify-content-center align-items-center gap-12 text-center">
-									<Link className="btn_icon btn44 dark-green-gradient" rel="noreferrer">
-										<span><RenderIconWithHover iconUrl={addBaseUrlToUrls(twitter?.image)} hoverIconUrl={"./img-1.svg"} defaultIcon={ICONS.Twitter} /></span>
-									</Link>
-									<Link className="btn_icon btn44 dark-green-gradient" rel="noreferrer">
-										<span><RenderIconWithHover iconUrl={addBaseUrlToUrls(discord?.image)} hoverIconUrl={"./img-1.svg"} defaultIcon={ICONS.Discord} /></span>
-									</Link>
-									<Link className="btn_icon btn44 dark-green-gradient" rel="noreferrer">
-										<span>
-											<RenderIconWithHover iconUrl={addBaseUrlToUrls(telegram?.image)} hoverIconUrl={"./img-1.svg"} defaultIcon={ICONS.Telegram} />
-										</span>
-									</Link>
-								</div>
+
+							<div className="w-100 d-flex justify-content-between flex-column-reverse flex-md-row align-items-center gap-12 header-button">
+								<button type="button" className="btn btn-md btn-primary">
+									<span className="transform-none"><RenderIconWithHover iconUrl={addBaseUrlToUrls(telegram?.image)} hoverIconUrl={"./img-1.svg"} defaultIcon={ICONS.Airdrop} /><SafeHTML html={airdropButton?.text} /></span>
+								</button>
+								<button type="button" className="btn btn-md btn-primary"><span className="transform-none"><SafeHTML html={viewChartButton?.text} /></span></button>
+								<button type="button" className="btn btn-md btn-secondary"><span className="text-white transform-none"><SafeHTML html={buyTokenButton?.text} /></span></button>
 							</div>
 						</div>
+					</div>
+				</div>
+
+
+				{/* Responsive Menu */}
+				<div className="mobileMenuList d-xl-none justify-content-between flex-column flex-xl-row align-items-center gap-12">
+
+					<div className="w-100 d-flex header_toggle">
+						<Link className="navbar-brand" to={"/"}>
+							<RenderIconWithHover iconUrl={addBaseUrlToUrls(premiumTokenTitle)} hoverIconUrl={"./img-1.svg"} defaultIcon={ICONS.Logo} />
+						</Link>
+
+						<button className="close_menu" onClick={toggleMenu}>
+							<svg width="20" height="20" viewBox="0 0 20 20" fill="none">
+								<path d="M1.66667 1.6665L18.3333 18.3332M1.66667 18.3332L18.3333 1.6665" stroke="#389989" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+							</svg>
+						</button>
+					</div>
+
+					<div className="w-100 header-button d-flex justify-content-between flex-column-reverse flex-md-row align-items-center gap-12 ms-md-auto">
+						<button type="button" className="btn btn-md btn-primary w-100">
+							<span className="transform-none"><RenderIconWithHover iconUrl={addBaseUrlToUrls(telegram?.image)} hoverIconUrl={"./img-1.svg"} defaultIcon={ICONS.Airdrop} /><SafeHTML html={airdropButton?.text} /></span>
+						</button>
+						<button type="button" className="btn btn-md btn-primary w-100"><span className="transform-none"><SafeHTML html={viewChartButton?.text} /></span></button>
+						<button type="button" className="btn btn-md btn-secondary w-100"><span className="text-white transform-none"><SafeHTML html={buyTokenButton?.text} /></span></button>
+					</div>
+
+					<div className="brand_banner">
+						<img width={378} height="316" className="img-fluid" src={ICONS.BrandFrame} alt="Brand" />
+					</div>
+
+					<div className="social-icon d-flex justify-content-center align-items-center gap-12">
+						<Link className="btn_icon btn44 dark-green-gradient" rel="noreferrer">
+							<span><RenderIconWithHover iconUrl={addBaseUrlToUrls(twitter?.image)} hoverIconUrl={"./img-1.svg"} defaultIcon={ICONS.Twitter} /></span>
+						</Link>
+						<Link className="btn_icon btn44 dark-green-gradient" rel="noreferrer">
+							<span><RenderIconWithHover iconUrl={addBaseUrlToUrls(discord?.image)} hoverIconUrl={"./img-1.svg"} defaultIcon={ICONS.Discord} /></span>
+						</Link>
+						<Link className="btn_icon btn44 dark-green-gradient" rel="noreferrer">
+							<span>
+								<RenderIconWithHover iconUrl={addBaseUrlToUrls(discord?.image)} hoverIconUrl={"./img-1.svg"} defaultIcon={ICONS.Discord} />
+							</span>
+						</Link>
 					</div>
 				</div>
 			</nav>
