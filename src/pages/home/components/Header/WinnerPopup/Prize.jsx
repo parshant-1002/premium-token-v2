9fc9bfe9bf743003cb81c75ca02365118dd15357
Davinder Singh <davinder.singh@chicmic.co.in>
2024-03-25 03:46:19 +0530

    fix: label in winner popup

--- src/pages/home/components/Header/WinnerPopup/Prize.jsx
+++ src/pages/home/components/Header/WinnerPopup/Prize.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { ICONS } from "../../../../../assets";
-import { DEFAULT_CONTENT } from "../helpers/constants";
 import CircularCheckBox from "../../../../../shared/components/CircularCheckBox";
 import './style.scss';
 
-export default function Prize({ prizeSelected, disabled, setPrizeSelected }) {
+export default function Prize({ prizeSelected, disabled, setPrizeSelected, label = "" }) {
     console.log('prizeSelected: ', prizeSelected);
     return <div className="prize_card">
         <div className="prize_wrap">
             <CircularCheckBox disabled={disabled} checked={prizeSelected} setChecked={setPrizeSelected} />
-            <span>{DEFAULT_CONTENT.PRIZE_LABEL}</span>
+            <span>{label}</span>
             <em className="win_icon"><img src={ICONS.premiumtoken} width={85} height={57} /></em>
         </div>
     </div>;
