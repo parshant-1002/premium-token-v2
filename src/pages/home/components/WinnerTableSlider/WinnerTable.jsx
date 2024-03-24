3c75c9c1ab17bd99997ebc9b3e4386a34c37b25c
Davinder Singh <davinder.singh@chicmic.co.in>
2024-03-25 04:50:44 +0530

    fix: winnertable

--- src/pages/home/components/WinnerTableSlider/WinnerTable.jsx
+++ src/pages/home/components/WinnerTableSlider/WinnerTable.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { getWinners } from '../../../../store/actions/contentManagement'
 import { STATUS } from '../../../../shared/constants'
@@ -12,11 +12,29 @@ const WinnerTable = ({ socket, prize }) => {
     const [skip, setSkip] = useState(0)
     const [totalCount, setTotalCount] = useState(null)
     const [tableData, setTableData] = useState([])
+    const limitRef = useRef(NUMBER_OF_ROWS_IN_PAGE*2)
     const dispatch = useDispatch()
     useEffect(() => {
         const payload = {
             skip,
-            limit: NUMBER_OF_ROWS_IN_PAGE
+            limit: limitRef.current
+        }
+        dispatch(getWinners(payload, (data, responseType) => {
+            if (responseType === STATUS.SUCCESS) {
+                const firstPageData = data?.data?.slice(0,NUMBER_OF_ROWS_IN_PAGE)
+                const secondPageData = data?.data?.slice(NUMBER_OF_ROWS_IN_PAGE)
+                setTableData((prev) => ([...prev, firstPageData, secondPageData]))
+                setTotalCount(data.totalCount)
+                setSkip(skip + NUMBER_OF_ROWS_IN_PAGE*2)
+                limitRef.current = NUMBER_OF_ROWS_IN_PAGE
+            }
+        }))
+    }, [])
+
+    function getWinnersData (skip){
+        const payload = {
+            skip,
+            limit: limitRef.current
         }
         dispatch(getWinners(payload, (data, responseType) => {
             if (responseType === STATUS.SUCCESS) {
@@ -24,34 +42,37 @@ const WinnerTable = ({ socket, prize }) => {
                 setTotalCount(data.totalCount)
             }
         }))
+    }
 
-    }, [skip])
+    // useEffect(() => {
+    //     if (socket) {
+    //         socket.on('latestWinner', (data) => {
+    //             if(data){
+    //                 setTableData((prevPages) => {
+    //                     const firstPage = [data?.data, ...prevPages[0]];
+    //                     let updatedPages = [firstPage.slice(0, NUMBER_OF_ROWS_IN_PAGE)];
+    //                     let remainingRows = firstPage.slice(NUMBER_OF_ROWS_IN_PAGE);
+    //                     for (let i = 1; i < prevPages.length; i++) {
+    //                         const page = [...remainingRows, ...prevPages[i]];
+    //                         updatedPages.push(page.slice(0, NUMBER_OF_ROWS_IN_PAGE));
+    //                         remainingRows = page.slice(NUMBER_OF_ROWS_IN_PAGE);
+    //                     }
+    //                     if (remainingRows.length > 0) {
+    //                         updatedPages.push(remainingRows);
+    //                     }
 
-    useEffect(() => {
-        if (socket) {
-            socket.on('latestWinner', (data) => {
-                setTableData((prevPages) => {
-                    const firstPage = [data?.data, ...prevPages[0]];
-                    let updatedPages = [firstPage.slice(0, NUMBER_OF_ROWS_IN_PAGE)];
-                    let remainingRows = firstPage.slice(NUMBER_OF_ROWS_IN_PAGE);
-                    for (let i = 1; i < prevPages.length; i++) {
-                        const page = [...remainingRows, ...prevPages[i]];
-                        updatedPages.push(page.slice(0, NUMBER_OF_ROWS_IN_PAGE));
-                        remainingRows = page.slice(NUMBER_OF_ROWS_IN_PAGE);
-                    }
-                    if (remainingRows.length > 0) {
-                        updatedPages.push(remainingRows);
-                    }
+    //                     return updatedPages;
+    //                 });
+    //                 setTotalCount(totalCount + 1);
+    //             }
 
-                    return updatedPages;
-                });
-                setTotalCount(data?.totalCount);
-            });
-        }
-    }, [socket]);
+    //         });
+    //     }
+    // }, [socket]);
 
     const handleNextClick = () => {
-        if ((totalCount === null || totalCount > skip * NUMBER_OF_ROWS_IN_PAGE)) {
+        if ((totalCount === null || totalCount > skip)) {
+            getWinnersData(skip)
             setSkip((prev) => prev + NUMBER_OF_ROWS_IN_PAGE)
         }
     }
@@ -100,8 +121,8 @@ const WinnerTable = ({ socket, prize }) => {
             }
           },
       ]
-    return (
-        <CustomSlick slidesToShow={1} handleNextClick={handleNextClick} responsive = {responsiveConfig}>
+    return (tableData?.length ?
+         <CustomSlick slidesToShow={1} handleNextClick={handleNextClick} responsive = {responsiveConfig}>
             {tableData?.map((pageData, index) => (
                 <table className="table table-borderless">
                     <thead>
@@ -121,7 +142,10 @@ const WinnerTable = ({ socket, prize }) => {
                 </table>
             ))}
 
-        </CustomSlick>
+        </CustomSlick> : 
+        <div>
+
+        </div>
     )
 }
 
