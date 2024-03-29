41621dc096ae5a16395a8698e8c16552cd771576
ankushchicmic <ankush.kumar@chicmic.co.in>
2024-03-29 11:33:32 +0530

    fix: feedback point changes

--- src/shared/components/RenderIconWithHover/RenderIcon.jsx
+++ src/shared/components/RenderIconWithHover/RenderIcon.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import "./RenderIcon.scss"
 const RenderIcon = ({iconUrl, hoverIconUrl, defaultIcon,defaultIconHover, iconAlt,onClick = ()=>{}, ...props}) => {
   return (
-      <div class="image_renderer" onClick={onClick}>
+      <div class="image_renderer" type="button" onClick={onClick}>
         <img src={iconUrl || defaultIcon} alt={iconAlt} {...props}/>
-        {hoverIconUrl && <img src={hoverIconUrl || defaultIconHover} class="img-top" alt={iconAlt} {...props} />} 
+        {hoverIconUrl && <img src={hoverIconUrl || defaultIconHover} class="img-top" alt={iconAlt} {...props} />}
     </div>
   )
 }
