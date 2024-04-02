9ac1d90da8d1d72d66fdf048140fe43bd8b732b8
chicmicdev <rohitkumar@chicmic.co.in>
2024-04-02 20:55:19 +0530

    Add d3 library and implement responsive font size

--- src/pages/home/components/DoughnutChart/DoughnutChart.jsx
+++ src/pages/home/components/DoughnutChart/DoughnutChart.jsx
@@ -1,18 +1,42 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import * as d3 from "d3";
 import { mapDonutChartData } from "../../helpers/constants";
 import { useSelector } from "react-redux";
 import { convertToAppropriateUnit, convertToLocale } from "../../../../shared/utilities";
-import "./style.scss"
+import "./style.scss";
+
+const calculateFontSize = (size) => {
+  if (window.innerWidth > 768) {
+    return `${size}px`; // Font size for larger screens
+  } else {
+    return `${size - 5}px`; // Font size for smaller screens
+  }
+};
+
 const MyD3Component = ({ series, innerTitle }) => {
   const ref = useRef();
   const blockChainData = useSelector((store) => store.contentManagementReducer.blockChainData);
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    const updateDimensions = () => {
+      setDimensions({
+        width: window.innerWidth > 768 ? 600 : window.innerWidth,
+        height: window.innerWidth > 768 ? 600 : 400
+      });
+    };
+
+    updateDimensions();
+
+    window.addEventListener("resize", updateDimensions);
+    return () => window.removeEventListener("resize", updateDimensions);
+  }, []);
 
   useEffect(() => {
     if (series && series.length) {
       const data = mapDonutChartData(series);
-      const height = window.innerWidth > 768 ? 600 : 400;
-      const width = window.innerWidth > 768 ? 600 : window.innerWidth;
+      const height = dimensions.height;
+      const width = dimensions.width;
       const margin = 50;
       const radius = Math.min(width, height) / 2 - margin;
 
@@ -53,8 +77,6 @@ const MyD3Component = ({ series, innerTitle }) => {
         .attr("fill", (d) => d.data.color)
         .attr("stroke", "rgb(17, 24, 39)")
         .style("stroke-width", "2px")
-        // .style("opacity", 0.7)
-        // Add hover animation
         .on("mouseover", function () {
           d3.select(this)
             .transition()
@@ -77,25 +99,21 @@ const MyD3Component = ({ series, innerTitle }) => {
         .attr("stroke", "#fff")
         .style("fill", "none")
         .attr("stroke-width", 1)
-        .style("stroke-dasharray", ("4, 4")) // Make the line dotted
+        .style("stroke-dasharray", "4, 4")
         .attr("points", (d) => {
           const posA = outerArc.centroid(d);
           const posB = outerArc2.centroid(d);
           const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2;
-          const angle = midangle < Math.PI ? 1 : -1; // Determine the angle based on the position of posB
+          const angle = midangle < Math.PI ? 1 : -1;
 
-          // Calculate the distance between posB and posC
           const distanceBC = Math.sqrt((posA[0] - posB[0]) ** 2 + (posA[1] - posB[1]) ** 2) + 20;
 
-          // Determine the direction of posC based on the angle
           const posC = angle === 1 ?
-            [posB[0] + distanceBC, posB[1]] :  // If angle is on the left side, position to the left
-            [posB[0] - distanceBC, posB[1]];   // If angle is on the right side, position to the right
+            [posB[0] + distanceBC, posB[1]] :
+            [posB[0] - distanceBC, posB[1]];
           posB[0] = posB[0] + angle * 15;
           return [posA, posB, posC];
         });
-
-
 
       svg
         .selectAll("text")
@@ -104,31 +122,31 @@ const MyD3Component = ({ series, innerTitle }) => {
         .append("text")
         .attr("fill", "#fff")
         .attr("y", -6)
+        .attr("font-size", calculateFontSize(16))
         .text((d) => `${d.data.value}%`)
         .attr("transform", (d) => {
           const posA = outerArc.centroid(d);
           const posB = outerArc2.centroid(d);
           const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2;
-          const angle = midangle < Math.PI ? 1 : -1; // Determine the angle based on the position of posB
-          // Calculate the distance between posB and posC
-          const distanceBC = Math.sqrt((posA[0] - posB[0]) ** 2 + (posA[1] - posB[1]) ** 2) - 15;
+          const angle = midangle < Math.PI ? 1 : -1;
 
-          // Determine the direction of posC based on the angle
+          const distanceBC = Math.sqrt((posA[0] - posB[0]) ** 2 + (posA[1] - posB[1]) ** 2) - (window.innerWidth > 768 ? 15 : 0);
+
           const posC = angle === 1 ?
-            [posB[0] + distanceBC, posB[1]] :  // If angle is on the left side, position to the left
-            [posB[0] - distanceBC, posB[1]];   // If angle is on the right side, position to the right
+            [posB[0] + distanceBC, posB[1]] :
+            [posB[0] - distanceBC, posB[1]];
           return `translate(${posC})`;
         })
         .style("text-anchor", (d) => {
           const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2;
           return midangle < Math.PI ? "start" : "end";
         });
-      // Add label and text on the center
+
       svg
         .append("text")
         .text(innerTitle || "")
         .attr("text-anchor", "middle")
-        .attr("font-size", "18px")
+        .attr("font-size", calculateFontSize(18))
         .attr("font-weight", "800")
         .attr("y", -10)
         .style("fill", "#40d1b4");
@@ -137,17 +155,17 @@ const MyD3Component = ({ series, innerTitle }) => {
         .append("text")
         .text(blockChainData?.tokenSupply ? convertToLocale(blockChainData?.tokenSupply) : '')
         .attr("text-anchor", "middle")
-        .attr("font-size", "24px")
+        .attr("font-size", calculateFontSize(24))
         .attr("font-weight", "800")
         .attr("y", 20)
         .style("fill", "white");
-      // Append a path element for the dotted line border
+
       svg.append("path")
         .attr("class", "chart-border")
         .attr("fill", "none")
         .attr("stroke", "#fff")
         .attr("stroke-width", "1.3")
-        .style("stroke-dasharray", ("4, 4")) // Make the line dotted
+        .style("stroke-dasharray", "4, 4")
         .attr("d", d3.arc()({
           outerRadius: radius * .85,
           startAngle: 0,
@@ -164,7 +182,7 @@ const MyD3Component = ({ series, innerTitle }) => {
           endAngle: Math.PI * 2
         }));
     }
-  }, [series, blockChainData]);
+  }, [series, blockChainData, dimensions]);
 
   return <div className="d3_container" ref={ref} />;
 };
