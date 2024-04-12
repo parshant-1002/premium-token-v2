e01e161f842ae310c58dcb25f411b851c42d9754
chicmicdev <rohitkumar@chicmic.co.in>
2024-04-12 13:47:22 +0530

    feat: added show and hide content option

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,41 +1,46 @@
 // componentsdiv
-import { Header } from "./components/Header";
-import { InformationSection } from "./components/InformationSection";
-import { DataAggregator } from "./components/DataAggregator";
-import { WinnerSection } from "./components/WinnerSection";
-import { RoadMap } from "./components/RoadMap";
-import { ConnectionCreator } from "./components/ConnectionCreator";
-import { ConnectionMaker } from "./components/ConnectionMaker";
-import { Featured } from "./components/Featured";
-import { MarketPlace } from "./components/MarketPlace";
-import { Tokeninformation } from "./components/Tokeninformation";
-import { Partners } from "./components/Partners";
-import { Footer } from "./components/Footer";
-import { SocialMedia } from "./components/SocialMedia";
-import VideoSection from "./components/VideoSection";
-import { PremiumToken } from "./components/PremiumToken";
-import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { getContent, setSocketData } from "../../store/actions/contentManagement";
-import { DidYouWin } from "./components/DidYouWin";
-import Airdrop from "./components/Tokeninformation/Airdrop";
-import useSocket from "../../shared/hooks/useSocket";
-import { STATUS } from "../../shared/constants";
-import { DEFAULT_CONTENT, SectionTypes } from "./helpers/contentManagement";
-import { merge } from "lodash";
+import { Header } from './components/Header';
+import { InformationSection } from './components/InformationSection';
+import { DataAggregator } from './components/DataAggregator';
+import { WinnerSection } from './components/WinnerSection';
+import { RoadMap } from './components/RoadMap';
+import { ConnectionCreator } from './components/ConnectionCreator';
+import { ConnectionMaker } from './components/ConnectionMaker';
+import { Featured } from './components/Featured';
+import { MarketPlace } from './components/MarketPlace';
+import { Tokeninformation } from './components/Tokeninformation';
+import { Partners } from './components/Partners';
+import { Footer } from './components/Footer';
+import { SocialMedia } from './components/SocialMedia';
+import VideoSection from './components/VideoSection';
+import { PremiumToken } from './components/PremiumToken';
+import { useEffect, useMemo, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  getContent,
+  setSocketData,
+} from '../../store/actions/contentManagement';
+import { DidYouWin } from './components/DidYouWin';
+import Airdrop from './components/Tokeninformation/Airdrop';
+import useSocket from '../../shared/hooks/useSocket';
+import { STATUS } from '../../shared/constants';
+import { DEFAULT_CONTENT, SectionTypes } from './helpers/contentManagement';
+import { merge } from 'lodash';
 const Home = () => {
-  const[content, setContent] = useState(DEFAULT_CONTENT)
-  const dispatch = useDispatch()
+  const [content, setContent] = useState(DEFAULT_CONTENT);
+  const dispatch = useDispatch();
   const socket = useSocket();
 
-  useEffect(()=> {
-    dispatch(getContent({},(data, status)=>{
-      if(status === STATUS.SUCCESS){
-        const mergedContent = merge({}, DEFAULT_CONTENT, data);
-        setContent(mergedContent)
-      }
-    }))
-  },[])
+  useEffect(() => {
+    dispatch(
+      getContent({}, (data, status) => {
+        if (status === STATUS.SUCCESS) {
+          const mergedContent = merge({}, DEFAULT_CONTENT, data);
+          setContent(mergedContent);
+        }
+      })
+    );
+  }, []);
 
   useEffect(() => {
     if (socket) {
@@ -45,32 +50,89 @@ const Home = () => {
     }
   }, [socket]);
 
-  const getContentData = ((sectionType)=>{
-    return content?.[sectionType]
-  })
-  
+  const getContentData = useMemo(() => (sectionType) => content?.[sectionType], [content]);
+
   return (
     <>
-      <Header content={getContentData(SectionTypes.HEADERS)} partnersContent={getContentData(SectionTypes.PARTNERS)} winnerPopup = {getContentData(SectionTypes.WINNER_POPUP)}/>
-      <VideoSection content={getContentData(SectionTypes.VIDEO_SECTION)} />
+      {getContentData(SectionTypes.HEADERS).isVisible && <Header
+        content={getContentData(SectionTypes.HEADERS)}
+        partnersContent={getContentData(SectionTypes.PARTNERS)}
+        winnerPopup={getContentData(SectionTypes.WINNER_POPUP)}
+      />}
+      {getContentData(SectionTypes.VIDEO_SECTION).isVisible && (
+        <VideoSection content={getContentData(SectionTypes.VIDEO_SECTION)} />
+      )}
       <div className="mobile_bg">
-        <InformationSection content={getContentData(SectionTypes.PRIZE_SECTION)}/>
-        <WinnerSection content={{ ...getContentData(SectionTypes.PRIZE_SECTION), ...getContentData(SectionTypes.WINNER_LIST_SECTION) }} socket={socket} />
+        {getContentData(SectionTypes.PRIZE_SECTION).isVisible && (
+          <InformationSection
+            content={getContentData(SectionTypes.PRIZE_SECTION)}
+          />
+        )}
+        {getContentData(SectionTypes.WINNER_LIST_SECTION).isVisible && (
+          <WinnerSection
+            content={{
+              ...getContentData(SectionTypes.PRIZE_SECTION),
+              ...getContentData(SectionTypes.WINNER_LIST_SECTION),
+            }}
+            socket={socket}
+          />
+        )}
       </div>
-      <DidYouWin content={getContentData(SectionTypes.WINNER_RULES_SECTION)} socket = {socket} />
-      <DataAggregator content={getContentData(SectionTypes.CONTRACT_DETAILS)} />
-      <RoadMap content={getContentData(SectionTypes.ROADMAP_SECTION)} />
-      <PremiumToken content={getContentData(SectionTypes.PREMIUM_TOKEN_WALLET)} />
-      <Featured content={getContentData(SectionTypes.FEATURE_SECTION)} />
-      <ConnectionCreator content={getContentData(SectionTypes.TOKEN_INFO)} />
-      <ConnectionMaker content={getContentData(SectionTypes.EXTERNAL_AUDIT)} />
-      <MarketPlace content={getContentData(SectionTypes.PREMIUM_MARKET_PLACE)} />
-      <Tokeninformation content={getContentData(SectionTypes.TOKEN_SUPPLY)} />
-      <Airdrop content={getContentData(SectionTypes.JOIN_AIRDROP)} />
+      {getContentData(SectionTypes.WINNER_RULES_SECTION).isVisible && (
+        <DidYouWin
+          content={getContentData(SectionTypes.WINNER_RULES_SECTION)}
+          socket={socket}
+        />
+      )}
+      {getContentData(SectionTypes.CONTRACT_DETAILS).isVisible && (
+        <DataAggregator
+          content={getContentData(SectionTypes.CONTRACT_DETAILS)}
+        />
+      )}
+      {getContentData(SectionTypes.ROADMAP_SECTION).isVisible && (
+        <RoadMap content={getContentData(SectionTypes.ROADMAP_SECTION)} />
+      )}
+      {getContentData(SectionTypes.PREMIUM_TOKEN_WALLET).isVisible && (
+        <PremiumToken
+          content={getContentData(SectionTypes.PREMIUM_TOKEN_WALLET)}
+        />
+      )}
+      {getContentData(SectionTypes.FEATURE_SECTION).isVisible && (
+        <Featured content={getContentData(SectionTypes.FEATURE_SECTION)} />
+      )}
+      {getContentData(SectionTypes.TOKEN_INFO).isVisible && (
+        <ConnectionCreator content={getContentData(SectionTypes.TOKEN_INFO)} />
+      )}
+      {getContentData(SectionTypes.EXTERNAL_AUDIT).isVisible && (
+        <ConnectionMaker
+          content={getContentData(SectionTypes.EXTERNAL_AUDIT)}
+        />
+      )}
+      {getContentData(SectionTypes.PREMIUM_MARKET_PLACE).isVisible && (
+        <MarketPlace
+          content={getContentData(SectionTypes.PREMIUM_MARKET_PLACE)}
+        />
+      )}
+      {getContentData(SectionTypes.TOKEN_SUPPLY).isVisible && (
+        <Tokeninformation content={getContentData(SectionTypes.TOKEN_SUPPLY)} />
+      )}
+      {getContentData(SectionTypes.JOIN_AIRDROP).isVisible && (
+        <Airdrop content={getContentData(SectionTypes.JOIN_AIRDROP)} />
+      )}
       {/* <Partners content={getContentData(SectionTypes.PARTNERS)} /> */}
-      <SocialMedia content={getContentData(SectionTypes.FOOTERS)} />
-      <Footer content={{ ...getContentData(SectionTypes.FOOTERS), ...getContentData(SectionTypes.HEADERS) }} partnersContent={getContentData(SectionTypes.PARTNERS)} />
-      </>
+      {getContentData(SectionTypes.FOOTERS).isVisible && (
+        <SocialMedia content={getContentData(SectionTypes.FOOTERS)} />
+      )}
+      {getContentData(SectionTypes.FOOTERS).isVisible && (
+        <Footer
+          content={{
+            ...getContentData(SectionTypes.FOOTERS),
+            ...getContentData(SectionTypes.HEADERS),
+          }}
+          partnersContent={getContentData(SectionTypes.PARTNERS)}
+        />
+      )}
+    </>
   );
 };
 
--- src/pages/home/components/WinnerTableSlider/WinnerTable.jsx
+++ src/pages/home/components/WinnerTableSlider/WinnerTable.jsx
@@ -1,166 +1,191 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { getWinners } from '../../../../store/actions/contentManagement'
-import { STATUS } from '../../../../shared/constants'
-import { NUMBER_OF_ROWS_IN_PAGE, WINNER_HEADINGS } from './helpers/constants'
-import { CustomSlick } from '../../../../shared/components/CustomSlick'
-import { addBaseUrlToUrls } from '../../../../shared/utilities'
-import { ICONS } from '../../../../assets'
-import { transformRow } from './helpers/utils'
-import SafeHTML from '../../../../shared/components/SanitizeHtml'
+import React, { useEffect, useRef, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { getWinners } from '../../../../store/actions/contentManagement';
+import { STATUS } from '../../../../shared/constants';
+import { NUMBER_OF_ROWS_IN_PAGE, WINNER_HEADINGS } from './helpers/constants';
+import { CustomSlick } from '../../../../shared/components/CustomSlick';
+import { addBaseUrlToUrls } from '../../../../shared/utilities';
+import { ICONS } from '../../../../assets';
+import { transformRow } from './helpers/utils';
+import SafeHTML from '../../../../shared/components/SanitizeHtml';
 
 const WinnerTable = ({ socket, prize, content }) => {
-    const [skip, setSkip] = useState(0)
-    const [totalCount, setTotalCount] = useState(null)
-    const [tableData, setTableData] = useState([])
-    const limitRef = useRef(NUMBER_OF_ROWS_IN_PAGE*2)
-    const dispatch = useDispatch()
-    useEffect(() => {
-        const payload = {
-            skip,
-            limit: limitRef.current
-        }
-        dispatch(getWinners(payload, (data, responseType) => {
-            if (responseType === STATUS.SUCCESS) {
-                if (data?.data?.length) {
-                const firstPageData = data?.data?.slice(0,NUMBER_OF_ROWS_IN_PAGE)
-                const secondPageData = data?.data?.slice(NUMBER_OF_ROWS_IN_PAGE)
-                const dataToAppend = [firstPageData]
-                if(secondPageData?.length){
-                    dataToAppend.push(secondPageData)
-                }
-                setTableData((prev) => ([...prev, ...dataToAppend]))
-                setTotalCount(data.totalCount)
-                setSkip(skip + NUMBER_OF_ROWS_IN_PAGE*2)
-                limitRef.current = NUMBER_OF_ROWS_IN_PAGE
-                }
+  const [skip, setSkip] = useState(0);
+  const [totalCount, setTotalCount] = useState(null);
+  const [tableData, setTableData] = useState([]);
+  const limitRef = useRef(NUMBER_OF_ROWS_IN_PAGE * 2);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const payload = {
+      skip,
+      limit: limitRef.current,
+    };
+    dispatch(
+      getWinners(payload, (data, responseType) => {
+        if (responseType === STATUS.SUCCESS) {
+          if (data?.data?.length) {
+            const firstPageData = data?.data?.slice(0, NUMBER_OF_ROWS_IN_PAGE);
+            const secondPageData = data?.data?.slice(NUMBER_OF_ROWS_IN_PAGE);
+            const dataToAppend = [firstPageData];
+            if (secondPageData?.length) {
+              dataToAppend.push(secondPageData);
             }
-        }))
-    }, [])
-
-    console.log(tableData,"tableDAta<><><><><>")
-    function getWinnersData (skip){
-        const payload = {
-            skip,
-            limit: limitRef.current
-        }
-        dispatch(getWinners(payload, (data, responseType) => {
-            if (responseType === STATUS.SUCCESS) {
-                if(data?.data?.length){
-                    setTableData((prev) => ([...prev, data?.data]))
-                    setTotalCount(data.totalCount)
-
-                }
-            }
-        }))
-    }
-
-    useEffect(() => {
-        if (socket) {
-            socket.on('latestWinner', (data) => {
-                if(data){
-                    setTableData((prevPages) => {
-                        const firstPage = [data?.data, ...prevPages[0]];
-                        let updatedPages = [firstPage.slice(0, NUMBER_OF_ROWS_IN_PAGE)];
-                        let remainingRows = firstPage.slice(NUMBER_OF_ROWS_IN_PAGE);
-                        for (let i = 1; i < prevPages.length; i++) {
-                            const page = [...remainingRows, ...prevPages[i]];
-                            updatedPages.push(page.slice(0, NUMBER_OF_ROWS_IN_PAGE));
-                            remainingRows = page.slice(NUMBER_OF_ROWS_IN_PAGE);
-                        }
-                        if (remainingRows.length > 0) {
-                            updatedPages.push(remainingRows);
-                        }
-
-                        return updatedPages;
-                    });
-                    setTotalCount(totalCount + 1);
-                }
-
-            });
-        }
-    }, [socket]);
-
-    const handleNextClick = () => {
-        if ((totalCount === null || totalCount > skip)) {
-            getWinnersData(skip)
-            setSkip((prev) => prev + NUMBER_OF_ROWS_IN_PAGE)
-        }
-    }
-    const renderTableRow = (row = {}) => {
-        const reformedRow = transformRow(row, true)
-        const getImageUrl = (prizeType)=>{
-            return addBaseUrlToUrls( prize?.find((item) => item.type == prizeType)?.imageUrl)
-        }
-        return (
-            <tr>
-                {WINNER_HEADINGS?.map((heading) => {
-                    if(heading.apiKey === "prizetype"){
-                        return <td data-label={heading.label} prizeType = {reformedRow[heading.apiKey]}>
-                            <div className='td_img'>
-                                <img src={getImageUrl(reformedRow[heading.apiKey])} />
-                            </div>
-                        </td>
-                    }
-                    return (
-                        <td data-label={heading.label}>{reformedRow[heading.apiKey]}</td>
-                    )
-                })}
-            </tr>
-        )
-    }
-
-    const responsiveConfig = [
-        {
-          breakpoint: 991,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            infinite: false,
-            arrows: false,
-            dots: true
+            setTableData((prev) => [...prev, ...dataToAppend]);
+            setTotalCount(data.totalCount);
+            setSkip(skip + NUMBER_OF_ROWS_IN_PAGE * 2);
+            limitRef.current = NUMBER_OF_ROWS_IN_PAGE;
           }
-        },
-        {
-            breakpoint: 575,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              infinite: false,
-              arrows: false,
-              dots: true
+        }
+      })
+    );
+  }, []);
+
+  console.log(tableData, 'tableDAta<><><><><>');
+  function getWinnersData(skip) {
+    const payload = {
+      skip,
+      limit: limitRef.current,
+    };
+    dispatch(
+      getWinners(payload, (data, responseType) => {
+        if (responseType === STATUS.SUCCESS) {
+          if (data?.data?.length) {
+            setTableData((prev) => [...prev, data?.data]);
+            setTotalCount(data.totalCount);
+          }
+        }
+      })
+    );
+  }
+
+  useEffect(() => {
+    if (socket) {
+      socket.on('latestWinner', (data) => {
+        if (data) {
+          setTableData((prevPages) => {
+            const firstPage = [data?.data, ...prevPages[0]];
+            let updatedPages = [firstPage.slice(0, NUMBER_OF_ROWS_IN_PAGE)];
+            let remainingRows = firstPage.slice(NUMBER_OF_ROWS_IN_PAGE);
+            for (let i = 1; i < prevPages.length; i++) {
+              const page = [...remainingRows, ...prevPages[i]];
+              updatedPages.push(page.slice(0, NUMBER_OF_ROWS_IN_PAGE));
+              remainingRows = page.slice(NUMBER_OF_ROWS_IN_PAGE);
             }
-          },
-      ]
-    return (tableData?.length ?
-         <CustomSlick slidesToShow={1} handleNextClick={handleNextClick} responsive = {responsiveConfig}>
-            {tableData?.map((pageData, index) => (
-                <table className="table table-borderless">
-                    <thead>
-                        <tr>
-                            {WINNER_HEADINGS.map((heading) => {
-                                return <th scope="col">{heading.label}</th>
-                            })}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            pageData?.map((item, index) => {
-                                return renderTableRow(item)
-                            })
-                        }
-                    </tbody>
-                </table>
-            ))}
+            if (remainingRows.length > 0) {
+              updatedPages.push(remainingRows);
+            }
 
-        </CustomSlick> :
-            <div className="d-flex justify-content-center flex-md-row flex-column align-items-center no-data-found">
-                <figure className="mb-0">
-                <img src={content?.noWinnerImageUrl} width={302} height={202} alt="No Data" />
-                </figure>
-            <p><SafeHTML html={content?.noWinnerText}/></p>
-            </div>
-    )
-}
+            return updatedPages;
+          });
+          setTotalCount(totalCount + 1);
+        }
+      });
+    }
+  }, [socket]);
 
-export default WinnerTable
+  const handleNextClick = () => {
+    if (totalCount === null || totalCount > skip) {
+      getWinnersData(skip);
+      setSkip((prev) => prev + NUMBER_OF_ROWS_IN_PAGE);
+    }
+  };
+  const renderTableRow = (row = {}, index) => {
+    const reformedRow = transformRow(row, true);
+    const getImageUrl = (prizeType) => {
+      return addBaseUrlToUrls(
+        prize?.find((item) => item.type == prizeType)?.imageUrl
+      );
+    };
+    return (
+      <tr key={`row-${index}`}>
+        {WINNER_HEADINGS?.map((heading, index) => {
+          if (heading.apiKey === 'prizetype') {
+            return (
+              <td
+                key={`${heading.label}-${index}`}
+                data-label={heading.label}
+                prizeType={reformedRow[heading.apiKey]}
+              >
+                <div className="td_img">
+                  <img src={getImageUrl(reformedRow[heading.apiKey])} />
+                </div>
+              </td>
+            );
+          }
+          return (
+            <td key={`${heading.label}-${index}`} data-label={heading.label}>
+              {reformedRow[heading.apiKey]}
+            </td>
+          );
+        })}
+      </tr>
+    );
+  };
+
+  const responsiveConfig = [
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        arrows: false,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        arrows: false,
+        dots: true,
+      },
+    },
+  ];
+  return tableData?.length ? (
+    <CustomSlick
+      slidesToShow={1}
+      handleNextClick={handleNextClick}
+      responsive={responsiveConfig}
+    >
+      {tableData?.map((pageData, index) => (
+        <table key={`slick-${index}`} className="table table-borderless">
+          <thead>
+            <tr>
+              {WINNER_HEADINGS.map((heading) => {
+                return (
+                  <th scope="col" key={heading.label}>
+                    {heading.label}
+                  </th>
+                );
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {pageData?.map((item, index) => {
+              return renderTableRow(item, index);
+            })}
+          </tbody>
+        </table>
+      ))}
+    </CustomSlick>
+  ) : (
+    <div className="d-flex justify-content-center flex-md-row flex-column align-items-center no-data-found">
+      <figure className="mb-0">
+        <img
+          src={content?.noWinnerImageUrl}
+          width={302}
+          height={202}
+          alt="No Data"
+        />
+      </figure>
+      <p>
+        <SafeHTML html={content?.noWinnerText} />
+      </p>
+    </div>
+  );
+};
+
+export default WinnerTable;
